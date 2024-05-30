555716055c72c7f267584142889cc52d66c6d7a0
FSusman <r.usman.tanveer@gmail.com>
2024-05-30 09:47:22 +0500

    WE NEED RENDER WHWHFDASJKFAKSDLJFWFWHAJS AHS HAHD AS

---
+++ server/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const courseRouter = require("./controllers/courses");
+
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+
+mongoose.set("strictQuery", false);
+
+logger.info("connecting to", config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI).then(() => {
+  logger.info("connected to MongoDB");
+});
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
+app.use('/api/courses', courseRouter)
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
